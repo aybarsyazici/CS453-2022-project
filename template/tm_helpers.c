54b4bf7fff42e4c0f3757da032377f3c20816b16
aybarsyazici <aybarsyazici@sabanciuniv.edu>
2022-11-06 17:42:00 +0100

    Split helper functions into its own file

---
+++ template/tm_helpers.c
@@ -0,0 +1,251 @@
+//
+// Created by Aybars Yazici on 6.11.2022.
+//
+
+#include "tm_helpers.h"
+
+void addReadSet(transaction *pTransaction, segment_node *pSegment, size_t offset, uint64_t version, void *value) {
+    read_set_node * newReadSet = (read_set_node *) malloc(sizeof(read_set_node)); // Create a new readSet node
+    newReadSet->segment = pSegment; // Which segment we read from
+    newReadSet->offset = offset; // Set the offset(how many words away from the start of the segment)
+    newReadSet->version = version; // The version at the time of reading
+    newReadSet->value = value; // The value we read
+    newReadSet->next = NULL; // The next is NULL as this is the newest node
+    if (pTransaction->readSetHead == NULL) { // If the head is NULL, then this is the first node
+        pTransaction->readSetHead = newReadSet;
+        pTransaction->readSetTail = newReadSet;
+        pTransaction->readSetSize = 1;
+    }
+    else { // If the head is not NULL, then we have to add the node to the end of the list
+        pTransaction->readSetTail->next = newReadSet;
+        pTransaction->readSetTail = newReadSet;
+        pTransaction->readSetSize++;
+    }
+}
+
+read_set_node *checkReadSet(Region *pRegion, transaction *pTransaction, void *address) {
+    read_set_node *currentReadSet = pTransaction->readSetHead;
+    while (currentReadSet != NULL) {
+        // Each read set node contains which segment it read from and the offset from the start of the segment
+        // Note that the offset is kept in terms of alignments(i.e. words), not bytes
+        // So to check if this current read set node read from the address we are looking for,
+        // we need to add the offset to the start of the segment and check if it is equal to the address.
+        if (currentReadSet->segment->freeSpace + currentReadSet->offset * pRegion->align == address) {
+            return currentReadSet;
+        }
+        currentReadSet = currentReadSet->next;
+    }
+    return NULL;
+}
+
+segment_node *findSegment(Region *pRegion, void *address) {
+    segment_node *currentSegment = pRegion->allocHead;
+    while (currentSegment != NULL) {
+        if (currentSegment->freeSpace <= address && address < currentSegment->freeSpace + currentSegment->size) {
+            return currentSegment;
+        }
+        currentSegment = currentSegment->next;
+    }
+    return NULL;
+}
+
+void addWriteSet(transaction *pTransaction, segment_node *pSegment, size_t offset, uint64_t version, void *value,
+                 void *newValue) {
+    write_set_node *newWriteSet = (write_set_node *) malloc(sizeof(write_set_node));
+    newWriteSet->segment = pSegment;
+    newWriteSet->offset = offset;
+    newWriteSet->version = version;
+    newWriteSet->value = value;
+    newWriteSet->newValue = newValue;
+    newWriteSet->next = NULL;
+    if (pTransaction->writeSetHead == NULL) {
+        pTransaction->writeSetHead = newWriteSet;
+        pTransaction->writeSetTail = newWriteSet;
+        pTransaction->writeSetSize = 1;
+    } else {
+        pTransaction->writeSetTail->next = newWriteSet;
+        pTransaction->writeSetTail = newWriteSet;
+        pTransaction->writeSetSize++;
+    }
+}
+
+write_set_node *checkWriteSet(Region *pRegion, transaction *pTransaction, void *address) {
+    write_set_node *currentWriteSet = pTransaction->writeSetHead;
+    while (currentWriteSet != NULL) {
+        // Each write set node contains which segment it wrote to and the offset from the start of the segment
+        // Note that the offset is kept in terms of alignments(i.e. words), not bytes
+        // So to check if this current write set node wrote to the address we are looking for,
+        // we need to add the offset to the start of the segment and check if it is equal to the address.
+        if (currentWriteSet->segment->freeSpace + currentWriteSet->offset * pRegion->align == address) {
+            return currentWriteSet;
+        }
+        currentWriteSet = currentWriteSet->next;
+    }
+    return NULL;
+}
+
+lock_t getLock(segment_node *pSegment, size_t offset) {
+    // Remember that each segment has one lock per TSM_WORDS_PER_LOCK words.
+    // the lock at index 'i' protects the words at indices i * TSM_WORDS_PER_LOCK to (i + 1) * TSM_WORDS_PER_LOCK - 1 (inclusive.)
+    // But imagine the segment has 12 words and TSM_WORDS_PER_LOCK is 5
+    // Then the segment will have 3 locks, and the lock at index 0 will protect the words at indices 0 to 4 (inclusive)
+    // The lock at index 1 will protect the words at indices 5 to 9 (inclusive)
+    // The lock at index 2 will protect the words at indices 10 to 11 (inclusive)
+    return pSegment->locks[offset / TSM_WORDS_PER_LOCK].lock;
+    // Notice how in our previous example all the indexes from 0 to 4 are divided by 5 and the result is 0
+    // For indexes 5 to 9, the result is 1, and for indexes 10 to 11, the result is 2, exactly as we want.
+}
+
+uint64_t getVersion(segment_node *pSegment, size_t offset) {
+    // Please take a look at getLock() to understand why we are doing this division to get the correct index.
+    return pSegment->locks[offset / TSM_WORDS_PER_LOCK].version;
+}
+
+lock_node getLockNode(segment_node *pSegment, size_t offset) {
+    // Please take a look at getLock() to understand why we are doing this division to get the correct index.
+    return pSegment->locks[offset / TSM_WORDS_PER_LOCK];
+}
+
+void sortWriteSet(transaction *pTransaction) {
+    write_set_node * currentWriteSetNode = pTransaction->writeSetHead;
+    write_set_node * nextWriteSetNode = NULL;
+    while(currentWriteSetNode != NULL){
+        nextWriteSetNode = currentWriteSetNode->next;
+        while(nextWriteSetNode != NULL){
+            lock_t currentLock = getLock(currentWriteSetNode->segment,currentWriteSetNode->offset);
+            lock_t nextLock = getLock(nextWriteSetNode->segment,nextWriteSetNode->offset);
+            if(&currentLock > &nextLock){
+                // Swap the write set nodes.
+                write_set_node temp = *currentWriteSetNode;
+                *currentWriteSetNode = *nextWriteSetNode;
+                *nextWriteSetNode = temp;
+            }
+            nextWriteSetNode = nextWriteSetNode->next;
+        }
+        currentWriteSetNode = currentWriteSetNode->next;
+    }
+}
+
+void releaseLocks(transaction *pTransaction) {
+    write_set_node *currentWriteSet = pTransaction->writeSetHead;
+    while (currentWriteSet != NULL) {
+        lock_t lock = getLock(currentWriteSet->segment, currentWriteSet->offset);
+        // Release the lock
+        lock_release(&lock);
+        currentWriteSet = currentWriteSet->next;
+    }
+}
+
+bool acquireLocks(transaction *pTransaction) {
+    // What we should watch out for is that multiple nodes may be trying to acquire the same lock.
+    // This is because one lock may protect multiple words in the same segment.
+    // So we need to make sure that we only acquire a lock once.
+    // We do this by keeping the track of the last segment and offset we acquired a lock for.
+    // If the current segment and offset are the same as the last one, we skip acquiring the lock.
+    segment_node* lastSegment;
+    size_t lastOffset;
+    bool firstLock = true;
+
+    write_set_node *currentWriteSet = pTransaction->writeSetHead;
+    while (currentWriteSet != NULL) {
+        lock_t currentLock = getLock(currentWriteSet->segment, currentWriteSet->offset);
+        // If this is the first lock we are trying to acquire, or if the current lock is different from the last lock we acquired
+        if (firstLock
+            || currentWriteSet->segment != lastSegment
+            || (lastOffset / TSM_WORDS_PER_LOCK) != (currentWriteSet->offset / TSM_WORDS_PER_LOCK)) {
+            // Try to acquire the lock
+            if (!lock_acquire(&currentLock)) {
+                // If we couldn't acquire the lock, release all the locks we acquired so far
+                releaseLocks(pTransaction);
+                return false;
+            }
+            // Update the last lock we acquired
+            lastSegment = currentWriteSet->segment;
+            lastOffset = currentWriteSet->offset;
+            firstLock = false;
+        }
+        currentWriteSet = currentWriteSet->next;
+    }
+    return true;
+}
+
+bool clearSets(transaction *pTransaction) {
+    // First clear the write set
+    write_set_node *currentWriteSet = pTransaction->writeSetHead;
+    write_set_node *nextWriteSet;
+    while (currentWriteSet != NULL) {
+        nextWriteSet = currentWriteSet->next;
+        free(currentWriteSet->newValue);
+        free(currentWriteSet->value);
+        free(currentWriteSet);
+        currentWriteSet = nextWriteSet;
+    }
+    pTransaction->writeSetHead = NULL;
+    pTransaction->writeSetTail = NULL;
+    pTransaction->writeSetSize = 0;
+    // Now clear the read set
+    read_set_node *currentReadSet = pTransaction->readSetHead;
+    read_set_node *nextReadSet;
+    while (currentReadSet != NULL) {
+        nextReadSet = currentReadSet->next;
+        free(currentReadSet->value);
+        free(currentReadSet);
+        currentReadSet = nextReadSet;
+    }
+    pTransaction->readSetHead = NULL;
+    pTransaction->readSetTail = NULL;
+    pTransaction->readSetSize = 0;
+    return true;
+}
+
+void incrementVersion(transaction *pTransaction, shared_t shared) {
+    // What we should watch out for is that multiple nodes may be trying to increment the version of the same lock.
+    // This is because one lock may protect multiple words in the same segment.
+    // So we need to make sure that we only increment the version of a lock once.
+    // We do this by keeping the track of the last segment and offset we incremented the version of.
+    // If the current segment and offset are the same as the last one, we skip incrementing the version.
+    segment_node* lastSegment;
+    size_t lastOffset;
+    bool firstLock = true;
+    Region *region = (Region *)shared;
+    if(unlikely(region == NULL)){
+        return;
+    }
+    uint64_t globalVersion = region->globalVersion;
+    write_set_node *currentWriteSet = pTransaction->writeSetHead;
+    while (currentWriteSet != NULL) {
+        lock_node currentLockNode = getLockNode(currentWriteSet->segment, currentWriteSet->offset);
+        // If this is the first lock we are trying to increment the version of, or if the current lock is different from the last lock we incremented the version of
+        if (firstLock
+            || currentWriteSet->segment != lastSegment
+            || (lastOffset / TSM_WORDS_PER_LOCK) != (currentWriteSet->offset / TSM_WORDS_PER_LOCK)) {
+            // Atomically set the new version equal to region globalVersion + 1
+            __atomic_store_n(&currentLockNode.version, globalVersion + 1, __ATOMIC_SEQ_CST);
+            // Update the last lock we incremented the version of
+            lastSegment = currentWriteSet->segment;
+            lastOffset = currentWriteSet->offset;
+            firstLock = false;
+        }
+        currentWriteSet = currentWriteSet->next;
+    }
+}
+
+void destroyTransaction(transaction *pTransaction) {
+    // Release all the locks associated with the transaction
+    releaseLocks(pTransaction);
+    // Clear the read and write sets
+    clearSets(pTransaction);
+    // Free the transaction
+    free(pTransaction);
+}
+
+transaction *getTransaction(Region *pRegion, tx_t tx) {
+    transaction* currentTransaction = pRegion->transactions;
+    while (currentTransaction != NULL) {
+        if (currentTransaction->id == tx) {
+            return currentTransaction;
+        }
+        currentTransaction = currentTransaction->prev;
+    }
+    return NULL;
+}
---
+++ template/tm_helpers.h
@@ -0,0 +1,147 @@
+//
+// Created by Aybars Yazici on 6.11.2022.
+//
+
+#ifndef CS453_2022_PROJECT_TM_HELPERS_H
+#define CS453_2022_PROJECT_TM_HELPERS_H
+#include "tsm_types.h"
+#include "macros.h"
+#include <stdlib.h>
+#endif //CS453_2022_PROJECT_TM_HELPERS_H
+
+
+/****************************************************************************************/
+/**                                                                                    **/
+/**  START OF HELPER FUNCTION DECLARATIONS                                             **/
+/**                                                                                    **/
+/****************************************************************************************/
+
+/** This function is used to get the transaction with the given id from the region.
+ * @param region The region to get the transaction from.
+ * @param id The id of the transaction to get.
+ * @return The transaction with the given id or NULL if no such transaction exists.
+ **/
+transaction* getTransaction(Region *pRegion, tx_t tx);
+
+/** This function is used to add a new readSet node to the given transaction.
+ * @param transaction The transaction to add the readSet node to.
+ * @param segment The memory segment that we are reading from.
+ * @param offset The number of alignments from the start of the segment.
+ * @param version The current version of the word that we are reading.
+ * @param value The value of the word that we are reading at read time
+ **/
+void addReadSet(transaction* pTransaction, segment_node* pSegment, size_t offset, uint64_t version, void* value);
+
+/** This function is used to check if a given address is inside the read set of a given transaction.
+ * @param shared The shared memory region that the address belongs to.
+ * @param transaction The transaction to check the read set of.
+ * @param address The address to check.
+ * @return The read set node that contains the address, or NULL if the address is not in the read set.
+ **/
+read_set_node* checkReadSet(Region* pRegion, transaction* pTransaction, void* address);
+
+/** This function is used to find the segment that contains the given address.
+ * @param shared The shared memory region that the address belongs to.
+ * @param address The address to find the segment of.
+ * @return The segment that contains the address, or NULL if the address is not in any segment.
+ **/
+segment_node* findSegment(Region* pRegion, void* address);
+
+/** This function is used to add a new writeSet node to the given transaction.
+ * @param transaction The transaction to add the writeSet node to.
+ * @param segment The memory segment that we are writing to.
+ * @param offset The number of alignments from the start of the segment.
+ * @param version The current version of the word that we are writing.
+ * @param value The value of the word that we are writing.
+ * @param newValue The new value of the word that we are writing.
+ **/
+void addWriteSet(transaction* pTransaction,
+                 segment_node* pSegment,
+                 size_t offset,
+                 uint64_t version,
+                 void* value,
+                 void* newValue);
+
+/** This function is used to check if a given address is inside the write set of a given transaction.
+ * @param shared The shared memory region that the address belongs to.
+ * @param transaction The transaction to check the write set of.
+ * @param address The address to check.
+ * @return The write set node that contains the address, or NULL if the address is not in the write set.
+ **/
+write_set_node* checkWriteSet(Region* pRegion, transaction* pTransaction, void* address);
+
+/** This function is used to get the lock associated with a memory location in a given segment
+ * @param segment The segment to get the lock from.
+ * @param offset The number of alignments from the start of the segment.
+ * @return The lock associated with the memory location.
+ **/
+lock_t getLock(segment_node* pSegment, size_t offset);
+
+/** This function is used to get the version number of a word in a given segment.
+ * @param segment The segment to get the version number from.
+ * @param offset The number of alignments from the start of the segment.
+ * @return The version number of the word.
+ **/
+uint64_t getVersion(segment_node* pSegment, size_t offset);
+
+/** This function is used to get the lock node of a word in a given segment.
+ * @param segment The segment to get the version number from.
+ * @param offset The number of alignments from the start of the segment.
+ * @return The version number of the word.
+ **/
+lock_node getLockNode(segment_node* pSegment, size_t offset);
+
+/** This function sorts the write set of the given transaction
+ * by increasing order of the address of locks it needs to acquire
+ * @param pTransaction pointer to the transaction
+ * @return void
+**/
+
+void sortWriteSet(transaction *pTransaction);
+
+/** This function is used to release all the locks of the given transaction
+ * We once again assume the write set is already sorted by increasing order of the address of locks.
+ * @param pTransaction The transaction to release the locks of.
+ * @return void
+ **/
+void releaseLocks(transaction* pTransaction);
+
+
+/** This function is used to acquire the locks associated with the write set of a given transaction.
+ * We assume that the given transaction's write set
+ * is sorted by increasing order of the address of locks it needs to acquire
+ * @param transaction The transaction to acquire the locks for.
+ * @return true if the locks were acquired successfully, or false if the locks couldn't be acquired.
+ **/
+bool acquireLocks(transaction* pTransaction);
+
+/** This function clears the read and write sets of the given transaction.
+ * @param transaction The transaction to clear the read and write sets of.
+ * @return true if the read and write sets were cleared successfully, or false if they couldn't be cleared.
+ **/
+bool clearSets(transaction* pTransaction);
+
+/** This function is used to increment the version of all lock nodes corresponding to the write set of a given transaction.
+ * We assume that the given transaction's write set
+ * is sorted by increasing order of the address of locks it needs to acquire
+ * @param transaction The transaction to increment the version of.
+ * @param shared The region the transaction belongs to
+ * @return void
+ **/
+void incrementVersion(transaction* pTransaction, shared_t shared);
+
+/** This function is used to destroy given transaction. It releases all the locks associated with the transaction,
+ * clears the read and write sets, and frees the transaction.
+ * @param transaction The transaction to destroy.
+ * @return void
+**/
+void destroyTransaction(transaction* pTransaction);
+
+/**
+ ***************************************************************************************
+ * END OF HELPER FUNCTIONS
+ ***************************************************************************************
+ **/
+
+
+
